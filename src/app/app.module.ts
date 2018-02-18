0dde85bfaf850de9f67353964409d3827512d7db
jerryfishzz <jerryfish@163.com>
2018-02-19 12:05:29 +1300

    just commit
    this branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DragulaModule } from 'ng2-dragula/ng2-dragula';
 
 import { AppComponent } from './app.component';
+import { DragulaComponent } from './dragula/dragula.component';
+import { OfficialComponent } from './official/official.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragulaComponent,
+    OfficialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/official/official.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
+
+@Component({
+  selector: 'app-official',
+  templateUrl: './official.component.html',
+  styleUrls: ['./official.component.css']
+})
+export class OfficialComponent implements OnInit {
+
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.drag.subscribe((value) => {
+      this.onDrag(value.slice(1));
+    });
+    dragulaService.drop.subscribe((value) => {
+      this.onDrop(value.slice(1));
+    });
+    dragulaService.over.subscribe((value) => {
+      this.onOver(value.slice(1));
+    });
+    dragulaService.out.subscribe((value) => {
+      this.onOut(value.slice(1));
+    });
+
+    dragulaService.setOptions('first-bag', {
+      revertOnSpill: true
+    });
+  }
+
+  private hasClass(el: any, name: string) {
+    return new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)').test(el.className);
+  }
+
+  private addClass(el: any, name: string) {
+    if (!this.hasClass(el, name)) {
+      el.className = el.className ? [el.className, name].join(' ') : name;
+    }
+  }
+
+  private removeClass(el: any, name: string) {
+    if (this.hasClass(el, name)) {
+      el.className = el.className.replace(new RegExp('(?:^|\\s+)' + name + '(?:\\s+|$)', 'g'), '');
+    }
+  }
+
+  private onDrag(args) {
+    let [e, el] = args;
+    this.removeClass(e, 'ex-moved');
+  }
+
+  private onDrop(args) {
+    let [e, el] = args;
+    this.addClass(e, 'ex-moved');
+  }
+
+  private onOver(args) {
+    let [e, el, container] = args;
+    this.addClass(el, 'ex-over');
+  }
+
+  private onOut(args) {
+    let [e, el, container] = args;
+    this.removeClass(el, 'ex-over');
+  }
+
+  ngOnInit() {
+  }
+
+}
